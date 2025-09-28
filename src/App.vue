7aa56bbfbd11769163deaa8899889f1ab63e688d
Laurien <laurien.santin@poatek.com>
2025-09-28 12:30:55 -0300

    adjusting style

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,12 @@ import Home from './components/Home.vue'
 </script>
 
 <template>
-  <header class="header">
-    <img src="./assets/marcaHorizontalBranca.png" alt="Sala d'Arme Cividini Marca Horizontal"/>
-  </header>
-  <div class="menu-nav">
-    <a href="#">
-      A Escola
-    </a>
-    <a href="#">
-      Aulas
-    </a>
-    <a href="#">
-      Contato
-    </a>
-  </div>
-  <Home msg="Vite + Vue" />
+  <div class="site">
+    <header class="header">
+      <img src="./assets/marcaHorizontalPreta.png" alt="Sala d'Arme Cividini Marca Horizontal"/>
+    </header>
+    <Home />
+  </div> 
 </template>
 
 <style scoped>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,21 +5,17 @@ import { ref } from 'vue'
 
 <template>
   <div class="home">
-    <h1>A Escola</h1>
     <div class="content">
-      <div class="column">
-        <img src="../assets/Fiore.jpg"/>
-        <footer>Fonte: https://wiktenauer.com/wiki/File:Fiore_delli_Liberi.jpg</footer>
-      </div>
-      <div class="column text">
-        <p>
-          A Sala d'Arme Cividini é uma escola de artes marciais focada em esgrima histórica, também conhecida pelo nome de HEMA (Artes Marciais Hitóricas Europeias). 
-          Com foco na tradição italiana, estudamos mestres como Fiore de'i Liberi, Salvator Fabris, Achille Marozzo, entre outros.
-        </p>
-        <p>
-          Temos aulas de Espada Longa, <i>Spada da Lato</i> e Rapieira. Venha nos conhecer!
-        </p>
-      </div>
+      <p>
+        A Sala d'Arme Cividini é uma escola de artes marciais focada em esgrima histórica, também conhecida pelo nome de HEMA (Artes Marciais Hitóricas Europeias). 
+        Com foco na tradição italiana, estudamos mestres como Fiore de'i Liberi, Salvator Fabris, Achille Marozzo, entre outros.
+      </p>
+      <p>
+        Temos aulas de Espada Longa, <i>Spada da Lato</i> e Rapieira. Venha nos conhecer!
+      </p>
+      <button class="know-more">
+        Conheça a Escola
+      </button>
     </div>
   </div>
 </template>
@@ -28,22 +24,26 @@ import { ref } from 'vue'
 p, h1 {
   text-align: center;
 }
-.content {
-  display: grid;
-  .column {
-    display: flex;
-    flex-flow: column;
-  }
+p {
+  font-size: 1rem;
+  color: #FFFFFF;
 }
-@media (min-width: 700px) {
-  .content { 
-    grid-template-columns: 1fr 2fr; 
-    gap: 1rem;
-    .text { 
-      display: flex;
-      justify-content: center; 
-      align-items: center;
-    }
-  }
+.home {
+  
+}
+.content {
+  background-color: rgba(75,75,75, 0.7);
+  display: grid;
+  justify-items: center;
+  align-content: center;
+}
+.know-more {
+  width: 8rem;
+  border: none;
+  border-radius: 0.3rem;
+  background-color: #4B4B4B;
+  color: #FFFFFF;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 </style>
